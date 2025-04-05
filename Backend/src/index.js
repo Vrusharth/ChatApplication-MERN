dfc074a4bc9f304370e94718ecd9489aa5f53359
Vrusharth Nirmal <vrusharth2003@gmail.com>
2025-04-05 22:44:19 +0530

    added socket done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,19 +2,23 @@
 import express from 'express';
 import authRoutes from './routes/authroute.js';
 import messageRoutes from './routes/messageroute.js'
+import { app,server} from './lib/socket.js'; // import socket.io server and express app
+
 import dotenv from 'dotenv';
 import connectDB from './lib/db.js'
 import cookieParser from 'cookie-parser';
 
-const app = express(); // create express app
+
 app.use(express.json()); // parse json data
 app.use(cookieParser()); // parse cookies
+
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
 dotenv.config(); // configure dotenv to use .env file
 const PORT=process.env.PORT || 5001; // set port to 5001
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log('Server is running on port',+ PORT);
     connectDB(); // connect to MongoDB kn
 })
